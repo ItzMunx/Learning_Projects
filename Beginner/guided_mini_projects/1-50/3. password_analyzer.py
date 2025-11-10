77c79ec656261e29aea5274e5cf1d3fbb0cac942
Demetrio Macias <Demetriogfx@gmail.com>
2025-11-09 20:42:40 -0800

    guided_syntax_drills 1

---
+++ Beginner/guided_mini_projects/1-50/3. password_analyzer.py
@@ -0,0 +1,26 @@
+# Goal
+# Create a program that checks a password entered by the user and rates its strength as:
+    # Weak, Moderate, or strong
+
+# Requirements
+# 1. User Input
+    # * Ask the user to enter a password:
+# 2. Validation Rules
+    # * Length          | Password must be 8 or more characters
+    # * Uppercase       | Must contain at least one uppercase letter
+    # * Lowercase       | Must Contain at least one lowercase letter
+    # * Digit           | Must contain at least on number
+    # * Special char    | Must Contain one special character (!@#$%^&*()_ etc)
+# 3. Assign 1 point per rule met
+
+# Desired output 
+# Password Analysis:
+# --------------------
+# Length: ✅
+# Uppercase: ❌
+# Lowercase: ✅
+# Digit: ✅
+#Special Character: ❌
+#
+#Overall Strength: Moderate
+
---
+++ Beginner/guided_syntax_drills/1-50/Variables & Data Types/1. create_three_variables.py
@@ -0,0 +1,11 @@
+# Create three variables:
+    # first_name (string)
+    # age (integer)
+    # height (float, in centimeters)
+
+# Name, age, and height
+first_name = "Demetrio"
+age = 29
+height = 180.34
+
+print(f"My name is {first_name}, I am {age} years old, and I am {height} cm tall.")
